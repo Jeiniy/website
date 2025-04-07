4a95a34b12a6014ff0fa64d96373b8ab8a67a484
Jeiniy <a0979679118@gmail.com>
2025-04-07 23:04:58 +0800

    4/7 統一上傳

--- SignUp/signup.js
+++ SignUp/signup.js
@@ -8,3 +8,18 @@ const navList = document.querySelector(".nav-list");
 menuToggle.addEventListener("click", function () {
     navList.classList.toggle("active"); // 切換「active」類別，控制選單顯示與隱藏
 });
+
+const mobileMenu = document.getElementById('mobile-menu');
+const overlay = document.getElementById('overlay');
+
+// 開啟選單
+mobileMenu.addEventListener('click', () => {
+    overlay.classList.add('active');
+});
+
+// 點擊 overlay 關閉選單
+overlay.addEventListener('click', (e) => {
+    if (e.target === overlay) { // 只點到背景才關閉
+        overlay.classList.remove('active');
+    }
+});
